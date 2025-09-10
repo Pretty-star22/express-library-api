23fc1d0c39a3570a3f4b071e650020ba66b26c2d
pretty peakanyo <peakanyopretty@gmail.com>
2025-09-10 15:22:08 +0200

    create server

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import { log } from "console"
+import express, {Express} from "express"
+
+const app:Express = express()
+
+const PORT =  process.env.PORT || 5000
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+    
+});
